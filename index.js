8c42b348b58f8ec3657ce10bc8284dd87ea519df
Triston <tristonherbst97@gmail.com>
2021-04-15 17:18:38 -0500

    commit tree 7a50dcd00c1fd71aa39d00df382ce16dc9594ccf

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  // tutorials.map(string => {
+  //  string.toLowerCase().split(' ').map(word => {
+  //    (word.charAt(0).toUpperCase() + word.slice(1))
+  //   }).join(' ')
+  // })
+
+  // tutorials.map(str => {
+  //   str.split(' ')
+  //   return str.map(word => {
+  //     return word.charAt(0).toUpperCase()
+  //   }).join(' ')
+    
+  // })
+
+  tutorials.map(str => {
+    return str.split(' ').toProperCase().join(' ')
+  })
 }
+
+// function titleCased() {
+//   tutorials.forEach(str => {
+//   return str.toLowerCase().split(' ').map(function(word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// })
+// }
+
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
